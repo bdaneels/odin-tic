ab921f5ff8a1313fae5ffb151236f3eced9ef5c1
bdaneels <brecht.daneels@hotmail.com>
2023-02-14 17:12:03 +0100

    add restart functionality and ui

--- script.js
+++ script.js
@@ -13,19 +13,28 @@ const Gameboard = (() => {
     div.addEventListener("click", () => GameController.playerClick(div), false);
   });
 
+  const reset = () => {
+    boardArray = [1, 2, 3, 4, 6, 7, 8, 9];
+    gameboardDivs.forEach((div) => {
+      div.innerHTML = "";
+    });
+  };
+
   return {
     getBoardArray,
     clear,
+    reset,
   };
 })();
 
 const Player = (name, symbol) => {
-  let turns = 3;
+  let turns = 4;
   const getName = () => name;
   const getTurns = () => turns;
+  const setTurns = (number) => (turns = number);
   const getSymbol = () => symbol;
   const subtractTurn = () => (turns -= 1);
-  return { getName, getTurns, getSymbol, subtractTurn };
+  return { getName, getTurns, getSymbol, subtractTurn, setTurns };
 };
 
 const playerOne = Player("Player One", "X");
@@ -38,6 +47,7 @@ const GameController = (() => {
     if (activePlayer === "") {
       index = Math.floor(Math.random() * playerArray.length);
       activePlayer = playerArray[index];
+      activePlayer.setTurns(5);
     } else if (activePlayer.getName() === playerOne.getName()) {
       activePlayer = playerTwo;
     } else if (activePlayer.getName() === playerTwo.getName()) {
@@ -63,8 +73,14 @@ const GameController = (() => {
       const [a, b, c] = winCombArray[i];
       if (array[a] === array[b] && array[b] === array[c]) {
         DisplayController.text(`${activePlayer.getName()} wins!`);
-        return true;
+        setTimeout(() => {
+          DisplayController.newGameQuery();
+        }, 900);
+        return "win";
       }
+    }
+    if (playerOne.getTurns() === 0 && playerTwo.getTurns() === 0) {
+      return "draw";
     }
   };
 
@@ -72,12 +88,21 @@ const GameController = (() => {
   const playerClick = (div) => {
     let index = div.getAttribute("data");
     let array = Gameboard.getBoardArray();
-    array[index] = GameController.getActivePlayer().getSymbol();
-    GameController.getActivePlayer().subtractTurn();
-    div.innerHTML = array[index];
-    GameController.checkWin();
-    if (!GameController.checkWin()) {
-      GameController.setActivePlayer();
+    if (array[index] === "X" || array[index] === "O") {
+      DisplayController.text("Can't click here, try again on an empty tile.");
+    } else {
+      array[index] = GameController.getActivePlayer().getSymbol();
+      GameController.getActivePlayer().subtractTurn();
+      div.innerHTML = array[index];
+      GameController.checkWin();
+      if (!GameController.checkWin()) {
+        GameController.setActivePlayer();
+      }
+      if (GameController.checkWin() === "draw") {
+        DisplayController.text(
+          "It's a draw! click 'new game' to start a new game!"
+        );
+      }
     }
   };
 
@@ -91,6 +116,26 @@ const GameController = (() => {
 
 const DisplayController = (() => {
   const scoreBoard = document.querySelector("#display");
+  const newGameDiv = document.querySelector("#new-game");
+  const newGameConfirmBtn = document.querySelector("#new-game-btn");
+  const newGameCancelBtn = document.querySelector("#new-game-btn-cancel");
+
+  const closeQuery = () => {
+    newGameDiv.classList.add("hidden");
+    newGameDiv.classList.remove("flex");
+    newGameDiv.classList.remove("absolute");
+  };
+
+  newGameCancelBtn.addEventListener("click", () => closeQuery(), false);
+
+  newGameConfirmBtn.addEventListener("click", () => {
+    playerOne.setTurns(4);
+    playerTwo.setTurns(4);
+    GameController.setActivePlayer();
+    Gameboard.reset();
+    closeQuery();
+  });
+
   const displayTurn = () => {
     scoreBoard.innerHTML =
       GameController.getActivePlayer().getName() + "'s turn.";
@@ -99,9 +144,16 @@ const DisplayController = (() => {
     scoreBoard.innerHTML = string;
   };
 
+  const newGameQuery = () => {
+    newGameDiv.classList.remove("hidden");
+    newGameDiv.classList.add("flex");
+    newGameDiv.classList.add("absolute");
+  };
+
   return {
     text,
     displayTurn,
+    newGameQuery,
   };
 })();
 
